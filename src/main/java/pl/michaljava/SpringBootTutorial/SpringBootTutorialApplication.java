fd367ec0929b01d9177498306b359a17bfc9ee2a
mnicinski <minidmnv@gmail.com>
2017-07-11 09:39:18 +0200

    commit inicjalizujacy - stan po otwarciu wygenerowanego projektu

---
+++ src/main/java/pl/michaljava/SpringBootTutorial/SpringBootTutorialApplication.java
@@ -0,0 +1,12 @@
+package pl.michaljava.SpringBootTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorialApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/michaljava/SpringBootTutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.michaljava.SpringBootTutorial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootTutorialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
